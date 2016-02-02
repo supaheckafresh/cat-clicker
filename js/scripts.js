b33894e0973e69a5a60a3e350892ae745032a5fa
supaheckafresh <neilstrain@gmail.com>
2016-02-02 15:20:26 -0800

    Start MVO structure and add placeholder data to localStorage

--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,61 @@
-/**
- * List of cats
- */
-var cats = {
-        lastClicked: null,
-        cats: [
-            { name: 'Sally', imgUrl: 'images/pants-cat.jpg', points: 0},
-            { name: 'Frank', imgUrl: 'images/paper-cat.jpg', points: 0},
-            { name: 'Brenard', imgUrl: 'images/box-cat.jpg', points: 0},
-            { name: 'Jennifer', imgUrl: 'images/luggage-cat.jpg', points: 0},
-            { name: 'Logan', imgUrl: 'images/hat-cat.jpg', points: 0},
-            { name: 'Grover', imgUrl: 'images/box-cat.jpg', points: 0},
-            { name: 'Licky', imgUrl: 'images/luggage-cat.jpg', points: 0},
-            { name: 'Mexico', imgUrl: 'images/hat-cat.jpg', points: 0}
-        ]
+
+$(function(){
+
+    var model = {
+
+        init: function () {
+
+            // placeholder data
+            if ( !localStorage.cats ) {
+                localStorage.cats = JSON.stringify(
+                    {
+                        lastClicked: null,
+
+                        // will use last id if implementing add new cat feature
+                        lastId: 8,
+                        cats: [
+                            { name: 'Sally', imgUrl: 'images/pants-cat.jpg', points: 0, id: 1},
+                            { name: 'Frank', imgUrl: 'images/paper-cat.jpg', points: 0, id: 2},
+                            { name: 'Brenard', imgUrl: 'images/box-cat.jpg', points: 0, id: 3},
+                            { name: 'Jennifer', imgUrl: 'images/luggage-cat.jpg', points: 0, id: 4},
+                            { name: 'Logan', imgUrl: 'images/hat-cat.jpg', points: 0, id: 5},
+                            { name: 'Grover', imgUrl: 'images/box-cat.jpg', points: 0, id: 6},
+                            { name: 'Licky', imgUrl: 'images/luggage-cat.jpg', points: 0, id: 7},
+                            { name: 'Mexico', imgUrl: 'images/hat-cat.jpg', points: 0, id: 8}
+                        ]
+                    }
+                )
+            }
+        },
+
+        getAllCats: function () {
+            return JSON.parse(localStorage.cats);
+        }
     };
 
-for (var i = 0, len = cats.cats.length; i < len; i++) {
-    cats.cats[i].id = 'cat' + (i + 1);
-}
 
-// call display cats function on page load
-displayCats();
+    var octopus = {
 
+        init: function () {
+            model.init();
+            view.init();
+        },
 
-/**
- * Display cat image, name and points for each cat in the list
- */
-function displayCats() {
-
-    // start by clearing out contents of #cats div
-    $('#cats').empty();
-
-    // go through list creating and displaying needed elements
-    for (var i = 0, length = cats.cats.length; i < length; i++ ) {
-        var cat = cats.cats[i];
-        var currentRow;
-
-        // create new row for each set of 3 cats including first row
-        if (i % 3 === 0) {
-            currentRow = 'r' + (i / 3);
-            $('<div/>', {class: 'row ' + currentRow}).appendTo('#cats');
+        getCats: function () {
+            return model.getAllCats();
         }
-
-        // append containing div for each cat
-        $('<div/>', {id: cat.id, class: 'col-xs-4'}).appendTo('.' + currentRow);
-
-        // display cat name
-        $('<h3/>').text(cat.name).appendTo('#' + cat.id);
-
-        // display cat image inside of div
-        $('<img/>', {src: cat.imgUrl, class: 'cat-img img-responsive'}).appendTo('#' + cat.id);
-
-        // display points
-        $('<h3/>').text("Points: " + cat.points).appendTo('#' + cat.id);
-    }
-}
+    };
 
 
-/**
- * Click listener for counting points when catDiv picture is clicked with mouse
- */
-$('#cats').on('click', '.cat-img', function updatePoints( evt ) {
+    var view = {
+        init: function () {
+            var cats = octopus.getCats();
+            console.log(cats);
+        }
+    };
 
-    // translate div id (format "cat1", "cat2", etc.) into index of cat in "cats" list (0 based).
-    var catIndex = $(this).closest('div').attr('id').slice(3) - 1;
-    var cat = cats.cats[catIndex];
+    octopus.init();
 
-    // store the id of the last cat clicked on
-    cats.lastClicked = cats.cats[catIndex].id;
-
-    // increase points for that cat when clicked with mouse
-    cat.points += 1;
-
-    // display updated points by replacing the next sibling of the cat image
-    $(this).next('h3').replaceWith('<h3>Points: ' + cat.points + '</h3>');
 });
+
+
