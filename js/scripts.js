76a044e232d8c04e76c5b5450e52350be5372e07
supaheckafresh <neilstrain@gmail.com>
2016-02-01 16:25:55 -0800

    Put display loop inside of displayCats() function

--- js/scripts.js
+++ js/scripts.js
@@ -6,34 +6,47 @@ var cats = [
     { name: 'Frank', imgUrl: 'images/paper-cat.jpg', id: 'cat' + 2, points: 0},
     { name: 'Brenard', imgUrl: 'images/box-cat.jpg', id: 'cat' + 3, points: 0},
     { name: 'Jennifer', imgUrl: 'images/luggage-cat.jpg', id: 'cat' + 4, points: 0},
-    { name: 'Logan', imgUrl: 'images/hat-cat.jpg', id: 'cat' + 5, points: 0}
+    { name: 'Logan', imgUrl: 'images/hat-cat.jpg', id: 'cat' + 5, points: 0},
+    { name: 'Grover', imgUrl: 'images/box-cat.jpg', id: 'cat' + 6, points: 0},
+    { name: 'Licky', imgUrl: 'images/luggage-cat.jpg', id: 'cat' + 7, points: 0},
+    { name: 'Mexico', imgUrl: 'images/hat-cat.jpg', id: 'cat' + 8, points: 0}
 ];
+
+// call display cats function on page load
+displayCats();
 
 
 /**
  * Display cat image, name and points for each cat in the list
  */
-for (var i = 0, length = cats.length; i < length; i++ ) {
-    var cat = cats[i];
-    var currentRow;
+function displayCats() {
 
-    // create new row for each set of 3 cats including first row
-    if (i % 3 === 0) {
-        currentRow = 'r' + (i / 3);
-        $('<div/>', {class: 'row ' + currentRow}).appendTo('#cats');
+    // start by clearing out contents of #cats div
+    $('#cats').empty();
+
+    // go through list creating and displaying needed elements
+    for (var i = 0, length = cats.length; i < length; i++ ) {
+        var cat = cats[i];
+        var currentRow;
+
+        // create new row for each set of 3 cats including first row
+        if (i % 3 === 0) {
+            currentRow = 'r' + (i / 3);
+            $('<div/>', {class: 'row ' + currentRow}).appendTo('#cats');
+        }
+
+        // append containing div for each cat
+        $('<div/>', {id: cat.id, class: 'col-xs-4'}).appendTo('.' + currentRow);
+
+        // display cat name
+        $('<h3/>').text(cat.name).appendTo('#' + cat.id);
+
+        // display cat image inside of div
+        $('<img/>', {src: cat.imgUrl, class: 'img-responsive'}).appendTo('#' + cat.id);
+
+        // display points
+        $('<h3/>').text("Points: " + cat.points).appendTo('#' + cat.id);
     }
-
-    // append containing div for each cat
-    $('<div/>', {id: cat.id, class: 'col-xs-4'}).appendTo('.' + currentRow);
-
-    // display cat name
-    $('<h3/>').text(cat.name).appendTo('#' + cat.id);
-
-    // display cat image inside of div
-    $('<img/>', {src: cat.imgUrl, class: 'img-responsive'}).appendTo('#' + cat.id);
-
-    // display points
-    $('<h3/>').text("Points: " + cat.points).appendTo('#' + cat.id);
 }
 
 
@@ -42,6 +55,7 @@ for (var i = 0, length = cats.length; i < length; i++ ) {
  */
 $('#cats').click( function updatePoints( evt ) {
 
+    // only do anything if image is selected
     if (evt.target.tagName.toLowerCase() === 'img') {
 
         // translate div id (format "cat1", "cat2", etc.) into index of cat in "cats" list (0 based).
@@ -52,6 +66,6 @@ $('#cats').click( function updatePoints( evt ) {
         cat.points += 1;
 
         // display updated points by replacing the next sibling of the cat image
-        $(evt.target).next().replaceWith('<h3>Points: ' + cat.points + '</h3>');
+        $(evt.target).next('h3').replaceWith('<h3>Points: ' + cat.points + '</h3>');
     }
 });
